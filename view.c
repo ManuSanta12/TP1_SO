f5722900fb6b680f59ba4a3d6c32e48d50c4c9e9
Manu <msantamarinabalbin@itba.edu.ar>
2024-04-08 10:16:21 -0300

    view can receive shared memory name via argv[]

--- view.c
+++ view.c
@@ -3,10 +3,14 @@
 int main(int argc, char* argv[]){
     char shm_name[MAX_NAME_SIZE];
     sleep(3);
-    //get semaphore name form stdin, sent by app process
-    int a = read(STDIN_FILENO,shm_name,MAX_NAME_SIZE);
-    shm_name[a] = '\0';
-    printf("bytes: %d read: %s\n", a,shm_name);
+    if(argc == 2){
+        strcpy(shm_name, argv[1]);
+    } else {
+        //get semaphore name form stdin, sent by app process
+        int a = read(STDIN_FILENO,shm_name,MAX_NAME_SIZE);
+        shm_name[a] = '\0';
+        printf("bytes: %d read: %s\n", a,shm_name);
+    }
 
     int shm_fd = shm_open(shm_name, O_RDONLY, S_IRUSR | S_IWUSR);
     if (shm_fd == -1){
