1cb49bdc5e02cd8d071eabb909e8bc265bad38a2
Manu <msantamarinabalbin@itba.edu.ar>
2024-04-11 14:47:46 -0300

    x

--- view.c
+++ view.c
@@ -1,35 +1,17 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "./include/lib.h"
 
-int main(int argc, char* argv[]){
-    char shm_name[MAX_NAME_SIZE];
-    if(argc == 2){
-        strcpy(shm_name, argv[1]);
-    } else {
-        //get semaphore name form stdin, sent by app process
-        int a = read(STDIN_FILENO,shm_name,MAX_NAME_SIZE);
-        shm_name[a] = '\0';
-        printf("bytes: %d read: %s\n", a,shm_name);
-    }
-
-    int shm_fd = shm_open(shm_name, O_RDONLY, S_IRUSR | S_IWUSR);
+int main(){
+    int shm_fd = shm_open(SHM_NAME, O_RDONLY, S_IRUSR | S_IWUSR);
     if (shm_fd == -1){
-        perror("Shared memory error in view process");
+        perror("Shared memory error");
         return EXIT_FAILURE;
     }
     char* buffer = mmap(NULL, BUFFER_SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
     sem_t* sem = sem_open(SEM_NAME, 0);
-    if(sem == SEM_FAILED){
-        perror("Semaphore error");
-        exit(0);
-    }
-    char re[READ_BUFFER_SIZE];
-    while(strcmp(re, END_MSG)!=0){
-        sem_wait(sem);
-        read(shm_fd, re, READ_BUFFER_SIZE);
-        printf("%s\n\n\n",re);    
-    }
+    sem_wait(sem);
+    printf("%s\n", buffer);
+    sem_post(sem);
+    printf("hola\n");
     munmap(buffer, BUFFER_SIZE);
     sem_close(sem);
     close(shm_fd);
