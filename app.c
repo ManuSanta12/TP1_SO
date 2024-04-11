9bdead6f9b3023c56f5cc8136a5f4966ca312b71
Manu <msantamarinabalbin@itba.edu.ar>
2024-04-11 16:04:57 -0300

    x

--- app.c
+++ app.c
@@ -35,53 +35,36 @@ int main(int argc, char *argv[]) {
                       ? SLAVES_QTY
                       : ((fileDeliveryInfo.fileQuantity + 1) / 2);
 
-
+                      
   // Arrays to hold file descriptors for pipes
   int appToSlaveFD[maxSlaves][NUMBER_OF_PIPE_ENDS];
   int slaveToAppFD[maxSlaves][NUMBER_OF_PIPE_ENDS];
   int pids[maxSlaves];
-
-
   // Create pipes for communication between parent process and child processes
   for (int nSlave = 0; nSlave < maxSlaves; nSlave++) {
     if (pipe(appToSlaveFD[nSlave]) != 0 || pipe(slaveToAppFD[nSlave]) != 0) {
       perror("Failed to create pipes");
     }
-    pids[nSlave] = fork();
-    if (pids[nSlave] == 0) {
+    if ((pids[nSlave] = fork()) == 0) {
       close(appToSlaveFD[nSlave][WRITE]);
       dup2(appToSlaveFD[nSlave][READ], STDIN_FILENO);
       close(appToSlaveFD[nSlave][READ]);
-      // close(appToSlaveFD[nSlave][READ]);
-
       close(slaveToAppFD[nSlave][READ]);
       dup2(slaveToAppFD[nSlave][WRITE], STDOUT_FILENO);
       close(slaveToAppFD[nSlave][WRITE]);
-      // close(slaveToAppFD[nSlave][WRITE]);
-
       for (int i = 0; i < nSlave; i++) {
         close(appToSlaveFD[i][WRITE]);
         close(slaveToAppFD[i][READ]);
       }
-      // for (int i = 0; i < nSlave; i++) {
-      //   close(appToSlaveFD[i][WRITE]);
-      //   close(slaveToAppFD[i][READ]);
-      // }
       execv("slave", (char *[]){"./slave", NULL});
-    } else if (pids[nSlave] > 0) {
-      close(slaveToAppFD[nSlave][WRITE]);
-      close(appToSlaveFD[nSlave][READ]);
-    } else {
-      perror("Error in fork");
     }
+    close(slaveToAppFD[nSlave][WRITE]);
+    close(appToSlaveFD[nSlave][READ]);
   }
-
-
   // Set up file descriptor set for reading
   fd_set readFDs;
   int quantity = amountToProcess(fileDeliveryInfo.fileQuantity,
                                  fileDeliveryInfo.deliveredFiles);
- 
   // Send files to slaves
   for (int i = 0; i < maxSlaves; i++) {
     for (int j = 0; j < quantity; j++) {
@@ -96,7 +79,6 @@ int main(int argc, char *argv[]) {
       fileDeliveryInfo.deliveredFiles++;
     }
   }
-
   // Open result file
   FILE *resultFile = fopen("result.txt", "w");
   if (resultFile == NULL) {
@@ -112,11 +94,9 @@ int main(int argc, char *argv[]) {
       if (slaveToAppFD[i][READ] > maxFD)
         maxFD = slaveToAppFD[i][READ];
     }
-
     if (select(maxFD + 1, &readFDs, NULL, NULL, NULL) == SELECT_ERROR) {
       perror("Error in select");
     }
-
     char buffer[SLAVE_BUFFER_SIZE * 2];
     char md5[SLAVE_BUFFER_SIZE];
     for (int i = 0; i < maxSlaves; i++) {
@@ -157,22 +137,16 @@ int main(int argc, char *argv[]) {
   }
   //sending message to end the view process.
   char end[sizeof(END_MSG)] = END_MSG;
-  write(shm_fd, end, sizeof(END_MSG));
   sem_post(sem);
-
+  write(shm_fd, end, sizeof(END_MSG));
   // Close pipes
   closePipes(appToSlaveFD, slaveToAppFD, maxSlaves);
-  
   munmap(map_result, BUFFER_SIZE);
   close(shm_fd);
   shm_unlink(SHM_NAME);
   sem_close(sem);
   sem_unlink(SEM_NAME);
-  
   // Free allocated memory
-   for (int i = 0; i < fileDeliveryInfo.fileQuantity; i++) {
-    free(paths[i]);
-  }
   free(paths);
   return 0;
 }
